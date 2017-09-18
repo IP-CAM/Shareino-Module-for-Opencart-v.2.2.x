db9a3db2e201cf426eb55b9c71cc78a20f9a04c2
AliRezaMohammadiDoost <alireza.mohammadi.1990@gmail.com>
2017-09-18 14:23:22 +0430

    رفع باگ

--- catalog/model/shareino/products.php
+++ catalog/model/shareino/products.php
@@ -9,7 +9,7 @@ class ModelShareinoProducts extends Model
 
         $products = array();
         foreach ($ids as $id) {
-            $this->getProductDetail($this->model_catalog_product->getProduct($id));
+            $products[] = $this->getProductDetail($this->model_catalog_product->getProduct($id));
         }
         return $products;
     }
@@ -21,18 +21,16 @@ class ModelShareinoProducts extends Model
         }
 
         $this->load->model('setting/setting');
+        $this->load->model('catalog/category');
+        $this->load->model('shareino/attribute');
+
         $website = $this->config->get('config_url') ?
             $this->config->get('config_url') : 'http://' . $_SERVER['SERVER_NAME'] . '/';
 
         $productId = $product['product_id'];
 
-        //$this->load->model('shareino/synchronize');
-        $this->load->model('shareino/attribute');
-        $this->load->model('catalog/category');
-
         $product_specials = $this->getProductSpecials($productId);
         $product_discounts = $this->getProductDiscounts($productId);
-        //$this->model_shareino_synchronize->synchronize($productId, $product['date_modified']);
 
         $listDiscounts = array();
         if ($product_specials) {
@@ -72,11 +70,14 @@ class ModelShareinoProducts extends Model
             }
         }
 
-        $attributesValues = $this->model_catalog_product->getProductAttributes($productId);
+        $attributesValues = $this->getProductAttributes($productId);
         $attributes = array();
-
         foreach ($attributesValues as $attr) {
-            $attribute = $this->model_catalog_attribute->getAttribute($attr['attribute_id']);
+            if (!isset($attr['attribute_id'])) {
+                break;
+            }
+
+            $attribute = $this->getAttribute();
             $attributes[$attribute['name']] = array(
                 'label' => $attribute['name'],
                 'value' => reset($attr['product_attribute_description'])['text']
@@ -251,6 +252,42 @@ class ModelShareinoProducts extends Model
         }
 
         return $product_category_data;
+    }
+
+    protected function getProductAttributes($product_id)
+    {
+        $product_attribute_group_data = array();
+
+        $product_attribute_group_query = $this->db->query("SELECT ag.attribute_group_id, agd.name FROM " . DB_PREFIX . "product_attribute pa LEFT JOIN " . DB_PREFIX . "attribute a ON (pa.attribute_id = a.attribute_id) LEFT JOIN " . DB_PREFIX . "attribute_group ag ON (a.attribute_group_id = ag.attribute_group_id) LEFT JOIN " . DB_PREFIX . "attribute_group_description agd ON (ag.attribute_group_id = agd.attribute_group_id) WHERE pa.product_id = '" . (int)$product_id . "' AND agd.language_id = '" . (int)$this->config->get('config_language_id') . "' GROUP BY ag.attribute_group_id ORDER BY ag.sort_order, agd.name");
+
+        foreach ($product_attribute_group_query->rows as $product_attribute_group) {
+            $product_attribute_data = array();
+
+            $product_attribute_query = $this->db->query("SELECT a.attribute_id, ad.name, pa.text FROM " . DB_PREFIX . "product_attribute pa LEFT JOIN " . DB_PREFIX . "attribute a ON (pa.attribute_id = a.attribute_id) LEFT JOIN " . DB_PREFIX . "attribute_description ad ON (a.attribute_id = ad.attribute_id) WHERE pa.product_id = '" . (int)$product_id . "' AND a.attribute_group_id = '" . (int)$product_attribute_group['attribute_group_id'] . "' AND ad.language_id = '" . (int)$this->config->get('config_language_id') . "' AND pa.language_id = '" . (int)$this->config->get('config_language_id') . "' ORDER BY a.sort_order, ad.name");
+
+            foreach ($product_attribute_query->rows as $product_attribute) {
+                $product_attribute_data[] = array(
+                    'attribute_id' => $product_attribute['attribute_id'],
+                    'name' => $product_attribute['name'],
+                    'text' => $product_attribute['text']
+                );
+            }
+
+            $product_attribute_group_data[] = array(
+                'attribute_group_id' => $product_attribute_group['attribute_group_id'],
+                'name' => $product_attribute_group['name'],
+                'attribute' => $product_attribute_data
+            );
+        }
+
+        return $product_attribute_group_data;
+    }
+
+    protected function getAttribute($attribute_id)
+    {
+        $query = $this->db->query("SELECT * FROM " . DB_PREFIX . "attribute a LEFT JOIN " . DB_PREFIX . "attribute_description ad ON (a.attribute_id = ad.attribute_id) WHERE a.attribute_id = '" . (int)$attribute_id . "' AND ad.language_id = '" . (int)$this->config->get('config_language_id') . "'");
+
+        return $query->row;
     }
 
 }
